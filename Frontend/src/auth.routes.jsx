60be0e8698278f5ea4d7027f0b80628512f65f7c
priyanshu-vaishnav <priyanshuvaishnav0606@gmail.com>
2026-06-18 13:01:12 +0530

    homepage insertion

--- Frontend/src/auth.routes.jsx
+++ Frontend/src/auth.routes.jsx
@@ -4,9 +4,14 @@ import Login from './features/auth/pages/Login.jsx'
 import Register from './features/auth/pages/Register.jsx'
 import Dashboard from './features/auth/pages/Dashboard.jsx'
 import InterviewReport from './features/api/pages/InterviewReport.jsx'
+import Home from './features/auth/pages/Home.jsx'
 
 export const router = createBrowserRouter([
  
+    {
+        path:"/",
+        element:<Home/>
+    }
     {
         path:"/Register",
         element:<Register/>
