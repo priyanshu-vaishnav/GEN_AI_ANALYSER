1df6f7dbbb84c8bebcb352b1c203e63e70d169ee
priyanshu-vaishnav <priyanshuvaishnav0606@gmail.com>
2026-06-18 13:21:34 +0530

    token & cookies samesite , secure = true

--- Backend/src/middleware/generateToken.js
+++ Backend/src/middleware/generateToken.js
@@ -10,7 +10,12 @@ async function genreateToken(res ,user) {
     }, process.env.JWSKEY, { expiresIn: '24h' })
 
     
-    res.cookie('token', token)
+    res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,      // KHOOB ZAROORI: Kyunki Render HTTPS use karta hai
+  sameSite: "none",   // SABSE IMPORTANT: Cross-domain (Vercel to Render) ke liye yeh 'none' hona hi chahiye
+  maxAge: 24 * 60 * 60 * 1000 // 1 din
+});
 
 
 
