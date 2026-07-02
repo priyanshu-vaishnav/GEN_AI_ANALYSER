607d08a645afcbbebdae3ffeede376b5faf686a7
priyanshu-vaishnav <priyanshuvaishnav0606@gmail.com>
2026-07-02 15:26:11 +0530

    login features added

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link ,useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { useAuth } from "../hooks/auth.hooks";
 import "./auth.forms.css";
 import { useState } from "react";
@@ -10,42 +10,52 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [isBlocked,setIsBlocked] = useState(true)
-  const [logoutError,setLogoutError] =useState(null)
+  const [isBlocked, setIsBlocked] = useState(true)
+  const [logoutError, setLogoutError] = useState(null)
 
 
-  const { user, loading, handleLogin, error, success,setError ,setSuccess} = useAuth();
+  const { user, loading, handleLogin, error, success, setError, setSuccess } = useAuth();
   const handleSubmit = (e) => {
     e.preventDefault();
     handleLogin({ email, password });
   };
 
-  
-  const openModal = ()=>{
+
+  const openModal = () => {
     console.log('press');
-   
+
     setIsBlocked(false)
-      
+
   }
 
-  const changePassword = async(password,confirmPassword)=>{
+  const changePassword = async (password, confirmPassword) => {
 
-    if(password !== confirmPassword){
-      console.log(password,confirmPassword)
+    if (password.toString() !== confirmPassword.toString()) {
+      setError("password is not matched !!")
+
+      setTimeout(() => {
+        setError("")
+      }, 2000);
       return;
     }
-       try{
-      const response = await axios.put("http://localhost:3000/api/auth/forgetpassword",{email,password})
+    try {
+      if (password.toString() !== confirmPassword.toString()) {
+
+        setError("password is not matched !!")
+        return;
+      }
+      const response = await axios.put("http://localhost:3000/api/auth/forgetpassword", { email, password })
       setSuccess(response.data.message)
-       }catch(err){
-        
-        setError(err.response.data.message)
-       }
-       finally{
+    } catch (err) {
+      setError(err.response.data.message)
+    }
+    finally {
+      setTimeout(() => {
         setError("")
         setSuccess("")
-       }
-      
+        setIsBlocked(true)
+      }, 2000);
+    }
   }
 
 
@@ -54,7 +64,7 @@ function Login() {
       <div className="form-box">
         <StatusScreen loading={loading} error={error} success={success} />
         <h3>Login</h3>
-       {isBlocked?<form>
+        {isBlocked ? <form>
           <div className="form-group">
             <label htmlFor="email">Email</label>
             <input
@@ -84,13 +94,13 @@ function Login() {
           >
             Login
           </button>
-          <div style={{marginTop:'10px'}}>
-          <button onClick={openModal}   className="btn btn-danger m-0 p-0" style={{backgroundColor:"#16181d",border:"none",}}>forget my password?</button>
+          <div style={{ marginTop: '10px' }}>
+            <button onClick={openModal} className="btn btn-danger m-0 p-0" style={{ backgroundColor: "#16181d", border: "none", }}>forget my password?</button>
           </div>
-          
-        </form>:
-            <div>
-              <label htmlFor="email">Email</label>
+
+        </form> :
+          <div>
+            <label htmlFor="email">Email</label>
             <input
               onChange={(e) => setEmail(e.target.value)}
               type="email"
@@ -98,31 +108,31 @@ function Login() {
               id="email"
               placeholder="Enter email"
             />
-             <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter password"
-            />
-          </div>
-             <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter password"
-            />
-          </div>
-
-              <button onClick={()=>changePassword(password,confirmPassword)}>submit</button>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter password"
+              />
             </div>
-          } 
-        
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter password"
+              />
+            </div>
+
+            <button onClick={() => changePassword(password, confirmPassword)}>submit</button>
+          </div>
+        }
+
         <p>
           Haven't an account? <Link to="/register">Register here</Link>
         </p>
